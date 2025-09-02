90ccb51718af6caec804a42a1d8e06d62b571052
Sallvainian <frank.cottone97@gmail.com>
2025-09-02 19:41:46 -0400

    chore(dev): move test_user_creation script out of lib to tools/scripts/

--- tools/scripts/test_user_creation.dart
+++ tools/scripts/test_user_creation.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'firebase_options.dart';
+import '../../lib/firebase_options.dart';
 
 /// Quick script to add test users to Firestore for testing user search
 Future<void> main() async {
